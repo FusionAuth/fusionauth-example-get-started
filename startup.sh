19a85ca034c426c946ae5c251327d36ded7d2f39
Kirsten Hunter <synedra@gmail.com>
2025-01-08 10:02:13 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 #!/bin/env
-echo "Waiting for the FusionAuth server to start up on http://localhost:9011"
-until curl --output /dev/null --silent --head --fail http://localhost:9011; do
-    printf "Checking for FusionAuth server...\n"
+echo "Waiting for the FusionAuth server to start up on http://localhost:9011."
+echo "When the server has started up you will get a terminal prompt and see the administrative login in the left hand pane of the browser window."
+until curl --output /dev/null --silent --head --fail http://localhost:9011/api/health; do
+    printf "."
     sleep 5
 done
 sleep 5
