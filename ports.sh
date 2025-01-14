01d62ca1966ed1b5f1620433f1eb9b55e81dd424
Kirsten Hunter <synedra@gmail.com>
2025-01-14 16:58:22 -0700

    Update ports.sh

--- ports.sh
+++ ports.sh
@@ -19,7 +19,6 @@ export REDIRECT_URL=`gp url 3000`
 export FUSIONAUTH_URL=`gp url 9011`
 export FUSIONAUTH_9012_URL=`gp url 9012`
 export FUSIONAUTH_8080=`gp url 8080`
-export FUSIONAUTH_1025=`gp url 1025`
 
 perl -pi -e 's#http://localhost:9011#$ENV{FUSIONAUTH_URL}#g' kickstart/kickstart.json app/src/index.ts app/.env kickstart/email-templates/*
 perl -pi -e 's#http://localhost:9012#$ENV{FUSIONAUTH_9012_URL}#g' kickstart/kickstart.json app/src/index.ts app/.env kickstart/email-templates/*
@@ -27,5 +26,4 @@ perl -pi -e 's#http://localhost:3000#$ENV{REDIRECT_URL}#g' kickstart/kickstart.j
 perl -pi -e 's#http://localhost:8080#$ENV{FUSIONAUTH_8080}#g' kickstart/kickstart.json app/src/index.ts app/.env kickstart/email-templates/*
 perl -pi -e 's#http://localhost:\{port\}#$ENV{FUSIONAUTH_8080}#g' kickstart/kickstart.json app/src/index.ts app/.env kickstart/email-templates/*
 
-perl -pi -e 's#mailcatcher#$ENV{FUSIONAUTH_1025}#g' kickstart/kickstart.json
-perl -pi -e 's#"port": 1025#"port":443#g' kickstart/kickstart.json
+perl -pi -e 's#mailcatcher#localhost#g' kickstart/kickstart.json
